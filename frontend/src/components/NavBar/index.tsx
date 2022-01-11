85b0447c18debad2626a63dd32c99df9922436e5
segredo-luciano <lucianosegredo20@gmail.com>
2022-01-10 22:28:33 -0300

    NavBar

---
+++ frontend/src/components/NavBar/index.tsx
@@ -0,0 +1,19 @@
+import {ReactComponent as GithubIcon} from 'assets/img/github.svg';
+import './styles.css';
+
+function NavBar() {
+
+    return(
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1> DSMovie </h1>
+                    <a href="https://github.com/segredo-luciano">
+                        <GithubIcon />
+                        <span className="git-user">/segredo-luciano</span>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    )
+} export default NavBar;
