c25ef556e6843075b42a3e2f394c94e3f535179b
segredo-luciano <lucianosegredo20@gmail.com>
2022-01-15 14:47:18 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,17 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { BASE_URL } from "utils/requests";
 
 function Listing() {
+
+    // wrong way
+    axios.get(`${BASE_URL}/movies`).then(
+        movie => {
+            console.log(movie.data);
+        }
+    )
+
     return (
         <div>
             <Pagination />
