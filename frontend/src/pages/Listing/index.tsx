10138e338fda65d6ed87dc046ee82e9fdb5eaab5
segredo-luciano <lucianosegredo20@gmail.com>
2022-01-15 15:39:44 -0300

    Props

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -9,50 +9,43 @@ function Listing() {
 
     const [pageNumber, setPageNumber] = useState(0);
 
+    const [page, setPage] = useState<MoviePage>({
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true,
+    });
+
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?page=1`).then(
-            movie => {
-                const data = movie.data as MoviePage;
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`).then(
+            movies => {
+                const data = movies.data as MoviePage;
                 console.log(data);
                 setPageNumber(data.number);
+                setPage(data)
             }
         )
-    }, []);
+        // When pageNumber changes, the request is made again
+    }, [pageNumber]);
 
     return (
         <div>
-            <p>{pageNumber}</p>
             <Pagination />
 
             <div className="container">
                 <div className="row">
-                    <div className="col-sm-6 col-lg-4 col-xl-3">
-                        <MovieCard />
-                    </div>
-
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
+                    {page.content.map(movie => {
+                        return (
+                            <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3">
+                                <MovieCard movie={movie} />
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
         </div>
